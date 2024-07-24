6f1eb59844ef2aa1cd0de3f575d6857aa4b5f5a6
cysabi <47790183+cysabi@users.noreply.github.com>
2024-07-23 20:02:08 -0400

    transition start

--- extension/index.js
+++ extension/index.js
@@ -1,27 +1,36 @@
 const OBSWebSocket = require("obs-websocket-js")
 
 const scenes = ["brb", "maplist", "casters"]
-const ip = `ws://localhost:4455`
+const ip = `ws://192.168.1.158:4455`
 
 module.exports = (nodecg) => {
   const scene = nodecg.Replicant("currentBreakScreen", "cq-dashboard", {
     defaultValue: 123,
   })
   const obs = new OBSWebSocket.default()
-
-  const setObsScene = () =>
-    obs.call("GetSceneList").then((data) => {
-      const newScene = scenes.find((scene) =>
-        data.currentProgramSceneName.toLowerCase().includes("#" + scene)
-      )
-      if (newScene) {
-        scene.value = newScene
-      }
-    })
-
   obs.connect(ip).then(() => {
+    const setObsScene = () =>
+      obs.call("GetSceneList").then((data) => {
+        const newScene = scenes.find((scene) =>
+          data.currentProgramSceneName.toLowerCase().includes("#" + scene)
+        )
+        if (newScene) {
+          scene.value = newScene
+        }
+      })
     obs.on("ConnectionOpened", () => {
       console.log(`Connected to OBS at ${ip}`)
+    })
+    obs.on("SceneTransitionStarted", () => {
+      obs.call("GetCurrentPreviewScene").then((data) => {
+        console.log(data)
+        const newScene = scenes.find((scene) =>
+          data.currentPreviewSceneName.toLowerCase().includes("#" + scene)
+        )
+        if (newScene) {
+          scene.value = newScene
+        }
+      })
     })
     obs.on("CurrentProgramSceneChanged", setObsScene)
     obs.on("Identified", setObsScene)
